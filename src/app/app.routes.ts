82753a45ed56cf4f0fe60d05401d99c7a4128e3c
Gustavo <109623134+Gustavo-Volso@users.noreply.github.com>
2025-06-27 22:24:39 -0300

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { FilaComponent } from './pedidos/fila/fila.component';
-
-export const appRoutes: Routes = [
-  { path: '', redirectTo: 'fila', pathMatch: 'full' },
-  { path: 'fila', component: FilaComponent }
-];
--- src/app/pedidos/models/pedido.ts
+++
@@ -1,6 +0,0 @@
-export interface Pedido {
-  id: number;
-  mesa: string;
-  itens: string;
-  status: 'pendente' | 'em preparo' | 'entregue';
-}
